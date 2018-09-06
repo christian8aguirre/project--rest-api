6076d777457e7d67ac1c3c9afb3e1678df842012
christian8aguirre <christian8.aguirre@icloud.com>
2018-09-05 22:46:01 -0500

    assignment 2

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 /**
- * Actvidad 1
+ * Actvidad 2
  * Author: Christian Aguirre
  */
 const express = require('express');
+const morgan = require('morgan');
+
+// const indexFile = `${__dirname}/app.js`
 
 const app = express();
 
-const port = 3000;
+const port = process.env.port || 3000;
+
+app.use(morgan('dev'));
+
 
 app.get('/',(req, res) =>{
-  res.send('01.  Basic Node Server');
+  res.send('Código ejecutándose correctamente...');
   })
+
+app.use((req, res)=>{
+  const err = '404. Not Found';
+
+  res.status(404).json(err)
+});
+
+app.use((req, res)=>{
+  const err = '500. Server error';
+  res.status(500).json(err);
+});
   
-app.listen(port, ()=>{console.log(`Aplicación ejecutándose en el puerto: ${port}` )});
+app.listen(port, ()=>{
+  console.log(`Aplicación ejecutándose en el puerto: ${port}` )
+});
